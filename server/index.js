c19b7457c14d88a0894e2a2298485d546b53343c
DERICKMALOBA <derickmaloba19@gmail.com>
2025-09-05 14:38:29 +0300

    node serving the html files

--- server/index.js
+++ server/index.js
@@ -47,5 +47,7 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
+app.use(express.static("public"));
+
 const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
